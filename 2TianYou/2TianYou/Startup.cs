5c1d85a6997cb9fc45d977a295e15fca15e61e18
lanmaodream <13073057993@163.com>
2016-09-16 11:39:18 +0800

    MVC空解决方案

---
+++ 2TianYou/2TianYou/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2TianYou.Startup))]
+namespace _2TianYou
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
